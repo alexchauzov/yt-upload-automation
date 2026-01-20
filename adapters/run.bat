f7dd01f57ba6eeb0950971201cf506b4684a4227
alexchauzov <achauzov@gmail.com>
2026-01-20 21:38:02 +0000

    Update run.bat with clarifying comments

--- adapters/run.bat
+++ adapters/run.bat
@@ -1,2 +1,3 @@
 python youtube_ui_uploader.py --file ../../.test_data/video1.mp4 --title "Test vodeo #1" --privacy scheduled --publish-at 2026-12-31T23:23:59Z --connect-cdp "http://127.0.0.1:9222"
-rem --chrome-profile "C:\Users\Alexander Chauzov\AppData\Local\Google\Chrome\User Data\Default"
+@rem --chrome-profile "C:\Users\Alexander Chauzov\AppData\Local\Google\Chrome\User Data\Default"
+@rem the parameter is commented becaude the only one option can be used at the same time --connect-cdp or --chrome-profile
