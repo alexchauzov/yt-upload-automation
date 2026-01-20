b2e743fa3d57af940b8b7acb5b0ee54c5d53896c
alexchauzov <achauzov@gmail.com>
2026-01-20 00:04:43 +0000

    [upd] delete chromium from the project

---
+++ adapters/browser.bat
@@ -0,0 +1,1 @@
+powershell & "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\YT-Automation"
--- adapters/run.bat
+++ adapters/run.bat
@@ -1,1 +1,2 @@
-python youtube_ui_uploader.py --file ../../.test_data/video1.mp4 --title "Test vodeo #1" --privacy scheduled --publish-at 2026-12-31T23:23:59Z
+python youtube_ui_uploader.py --file ../../.test_data/video1.mp4 --title "Test vodeo #1" --privacy scheduled --publish-at 2026-12-31T23:23:59Z --connect-cdp "http://127.0.0.1:9222"
+rem --chrome-profile "C:\Users\Alexander Chauzov\AppData\Local\Google\Chrome\User Data\Default"
